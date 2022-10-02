29d8e03e95299d896d72181e2cf0280c268b4dd8
Michael Turok <michael.turok@gmail.com>
2022-10-02 03:30:05 -0400

    Diamond row and different character

--- avi/rug.py
+++ avi/rug.py
@@ -4,7 +4,7 @@ def cube(rug, dim:int, row:int, col:int):
   for r in range(dim):
     for c in range(dim):
       rug[r+row][c+col] = 1
-  return row + dim
+  return row + dim - 1
 
 
 def line(rug, row:int):
@@ -22,13 +22,20 @@ def triangle(rug, dim:int, row:int, col:int, invert:bool=False):
     col_list = list(range(col-span, col+span+1))
     for c in col_list:
       rug[row+r][c] = 1
-  return row + dim
+  return row + dim - 1
 
 
 def diamond(rug, dim:int, row:int, col:int):
-  row = triangle(rug, dim, row,   col)
-  row = triangle(rug, dim, row-1, col, invert=True)
+  row = triangle(rug, dim, row, col)
+  row = triangle(rug, dim, row, col, invert=True)
   return row
+
+
+def diamond_row(rug, dim:int, row:int):
+  (height, width)= rug.shape
+  for c in range(dim, width - dim, 2 * dim-1):
+    last_row = diamond( rug, dim, row, c)
+  return last_row
 
 
 def triangle_row(rug, dim:int, row:int):
@@ -36,24 +43,24 @@ def triangle_row(rug, dim:int, row:int):
   invert = False   # we will alternate normal and inverted triangles
 
   for c in range(dim, width - dim, 2 * dim-1): # start/stop/step
-    triangle_row = triangle(rug, dim, row, c, invert=invert)
+    last_row = triangle(rug, dim, row, c, invert=invert)
     invert = not invert
-  return row + dim
+  return last_row
 
 
 def cube_row(rug, dim:int, row:int, col_start=0):
   # compute number of cubes
   (height, width)= rug.shape
   for c in range(col_start, width, dim*2): # start/stop/step
-    cube_row = cube(rug, dim, row, c)
-  return row + dim
+    last_row = cube(rug, dim, row, c)
+  return last_row
 
 
 def printme(rug):
   (height, width) = rug.shape
   for r in range(height):
     for c in range(width):
-      print('*' if rug[r][c] else ' ', end="")
+      print('%' if rug[r][c] else ' ', end="")
     print()
 
 
@@ -62,17 +69,17 @@ WIDTH = 30
 
 
 rug = np.zeros((HEIGHT, WIDTH))
-row = line(         rug, 0)
-row = line(         rug, row+1)
+row = line(         rug,    0)
+row = line(         rug,    row+1)
 row = triangle_row( rug, 3, row+2)
-row = line(         rug, row+1)
+row = line(         rug,    row+2)
 row = cube_row(     rug, 2, row+1)
-row = diamond(      rug, 4, row, 14)
-row = cube_row(     rug, 2, row)
-row = line(         rug, row)
+row = diamond_row(  rug, 4, row+1)
+row = cube_row(     rug, 2, row+1)
+row = line(         rug,    row+1)
 row = triangle_row( rug, 3, row+2)
-row = line(         rug, row+1)
-row = line(         rug, row+1)
+row = line(         rug,    row+2)
+row = line(         rug,    row+1)
 
 
 printme(rug)
