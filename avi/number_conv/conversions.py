d2ec8fbf4ec3aff20fc727edc5636856095801dd
Michael Turok <michael.turok@gmail.com>
2022-10-02 13:04:20 -0400

    Make this simpler

--- avi/number_conv/conversions.py
+++ avi/number_conv/conversions.py
@@ -4,7 +4,7 @@ import math
 
 
 def lookup_table():
-    return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A' , 'B', 'C', 'D', 'E', 'F']
+    return ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F']
 
 
 def to_decimal(input:str, base:int):
--- avi/rug/rug.py
+++ avi/rug/rug.py
@@ -1,12 +1,5 @@
 import numpy as np
 
-def cube(rug, dim:int, row:int, col:int): 
-  for r in range(dim):
-    for c in range(dim):
-      rug[r+row][c+col] = 1
-  return row + dim - 1
-
-
 def line(rug, row:int):
   (height, width)= rug.shape
   for c in range(width):
@@ -14,14 +7,19 @@ def line(rug, row:int):
   return row
 
 
+def cube(rug, dim:int, row:int, col:int):
+  for r in range(0, dim, 1):
+    for c in range(0, dim, 1):
+      rug[row+r][col+c] = 1
+  return row + dim - 1
+
+
 def triangle(rug, dim:int, row:int, col:int, invert:bool=False):
-  row_list = list(range(dim))
-  for r in row_list:
+  for r in range(0, dim, 1): # start/stop/step
     # compute the number of columns based on our row
-    span = r if not invert else dim - r - 1
-    col_list = list(range(col-span, col+span+1))
-    for c in col_list:
-      rug[row+r][c] = 1
+    col_width = r if not invert else dim - r - 1
+    for c in range(-col_width, col_width+1, 1): # start/stop/step
+      rug[row+r][col+c] = 1
   return row + dim - 1
 
 
@@ -31,10 +29,18 @@ def diamond(rug, dim:int, row:int, col:int):
   return row
 
 
+def cube_row(rug, dim:int, row:int, col_start=0):
+  # compute number of cubes
+  (height, width)= rug.shape
+  for c in range(col_start, width, dim*2): # start/stop/step
+    last_row = cube(rug, dim, row, c)
+  return last_row
+
+
 def diamond_row(rug, dim:int, row:int):
   (height, width)= rug.shape
-  for c in range(dim, width - dim, 2 * dim-1):
-    last_row = diamond( rug, dim, row, c)
+  for c in range(dim, width - dim, 2 * dim-1): # start/stop/step
+    last_row = diamond(rug, dim, row, c)
   return last_row
 
 
@@ -46,27 +52,19 @@ def triangle_row(rug, dim:int, row:int, invert:bool=False):
   return last_row
 
 
-def cube_row(rug, dim:int, row:int, col_start=0):
-  # compute number of cubes
-  (height, width)= rug.shape
-  for c in range(col_start, width, dim*2): # start/stop/step
-    last_row = cube(rug, dim, row, c)
-  return last_row
-
-
 def printme(rug):
   (height, width) = rug.shape
   for r in range(height):
     for c in range(width):
-      print('%' if rug[r][c] else ' ', end="")
+      print('#' if rug[r][c] else ' ', end="")
     print()
 
 
 HEIGHT = 30
 WIDTH = 30
 
-
 rug = np.zeros((HEIGHT, WIDTH))
+
 row = line(         rug,    0)
 row = line(         rug,    row+1)
 row = triangle_row( rug, 3, row+2, False)
@@ -79,6 +77,4 @@ row = triangle_row( rug, 3, row+2, True)
 row = line(         rug,    row+2)
 row = line(         rug,    row+1)
 
-
 printme(rug)
-
